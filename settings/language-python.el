5633da4e0ea847a6d4616f3246932e97b82ea5f4
ibnHatab <you@example.com>
2018-10-28 12:12:57 +0200

    pytautopep8

--- settings/language-python.el
+++ settings/language-python.el
@@ -37,5 +37,10 @@
              ))
 
 
+(require 'py-autopep8)
+(add-hook 'python-mode-hook 'py-autopep8-enable-on-save)
+
+(setq py-autopep8-options '("--max-line-length=100"))
+
 (provide 'language-python)
 ;;;
